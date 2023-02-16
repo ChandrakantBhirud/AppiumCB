cc3386732c48eb70e2562f9457f0137f484eb8c0
Chandrakant Bhirud <chandubhirud@yahoo.com>
2023-02-16 17:50:49 +0000

    test

--- steps/login.ts
+++ steps/login.ts
@@ -3,12 +3,7 @@ import loginpage from "../pages/loginPage";
 
 const lp = new loginpage();
 
-<<<<<<< HEAD
 //Minakshi 
-=======
-//Chandra B
-
->>>>>>> 34f1fcfeba6fbc407410b705660b1d37b5a8e196
 
 Given(/^Login applcaition with (.+) and (.+)$/, async(user, pwd)=>{
     await lp.login(user, pwd);  
