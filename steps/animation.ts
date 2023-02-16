202ca5d75fc4724031f5398eba97413277094ae5
Minakshi Bhirud <chandubhirud@yahoo.com>
2023-02-16 18:58:21 +0000

    gg

--- steps/animation.ts
+++ steps/animation.ts
@@ -1,12 +1,9 @@
 import { When } from "@wdio/cucumber-framework";
 import AnimationEvents from "../pages/Animation";
 
-<<<<<<< HEAD
 //chandu test
-=======
 //chandu bhai1
 //Minakshi test1
->>>>>>> main
 
 const AE = new AnimationEvents();
 
